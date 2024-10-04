5696cc8de0bca1ceb98458b64cfdf577d0e80692
rebootedCoder2 <oborimoon@gmail.com>
2024-10-04 14:03:46 +0900

    API_URL from page -> constants

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
-import { API_URL } from "../app/(home)/page";
+
+import { API_URL } from "../app/constants";
 import styles from "../styles/movie-info.module.css";
 export async  function getMovie(id:string) {
   // await new Promise((resolve) => setTimeout(resolve, 10000));
